43866fc17bd296fdeff225c5235aabd4f753c2d4
kostadinovs <stolek@outlook.com>
2024-02-12 19:40:33 +0100

    commit 8-1

---
+++ Lesson-8/script.js
@@ -0,0 +1,13 @@
+
+const button = document.getElementById('button');
+console.log(button);
+
+const buttons = document.getElementsByClassName('myButton');
+console.log(buttons);
+
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+button.innerHTML = '<b>Hello!</b>';
+// button.style.background = 'blue';
+// button.style.color = 'red';
